f9bb9d1ba5e1ba015ea0d2b11687dfbe26b2eba7
Sahil Jain <sjain3@caltech.edu>
2023-03-16 21:13:07 -0700

    finished data loading

--- load-data.sql
+++ load-data.sql
@@ -1,14 +1,16 @@
-LOAD DATA LOCAL INFILE 'seattle_hosts.csv' INTO TABLE airline
+SET GLOBAL local_infile=1;
+
+LOAD DATA LOCAL INFILE 'airline.csv' INTO TABLE airline
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS; -- If your CSV file has a row with column names
 
-LOAD DATA LOCAL INFILE 'seattle_hosts.csv' INTO TABLE route
+LOAD DATA LOCAL INFILE 'plane.csv' INTO TABLE plane
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS; -- If your CSV file has a row with column names
 
-LOAD DATA LOCAL INFILE 'seattle_hosts.csv' INTO TABLE plane
+LOAD DATA LOCAL INFILE 'airport.csv' INTO TABLE airport
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS; -- If your CSV file has a row with column names
 
-LOAD DATA LOCAL INFILE 'seattle_hosts.csv' INTO TABLE delay_info
+LOAD DATA LOCAL INFILE 'delay_info.csv' INTO TABLE delay_info
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS; -- If your CSV file has a row with column names
 
-LOAD DATA LOCAL INFILE 'seattle_hosts.csv' INTO TABLE airport
+LOAD DATA LOCAL INFILE 'routes.csv' INTO TABLE route
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS; -- If your CSV file has a row with column names
--- setup.sql
+++ setup.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS route;
+DROP TABLE IF EXISTS delay_info;
 DROP TABLE IF EXISTS airline;
 DROP TABLE IF EXISTS plane;
-DROP TABLE IF EXISTS delay_info;
 DROP TABLE IF EXISTS airport;
 
 -- Stores information about each airline
@@ -9,45 +9,11 @@ CREATE TABLE airline(
     -- the airline carrier code, which is of length 2 or 3
     carrier_code VARCHAR(3),
     -- the name of the airline
-    airline_name VARCHAR(100),
+    airline_name VARCHAR(200),
     -- the carrier code will be used to uniqely identify airlines
     PRIMARY KEY (carrier_code)
 );
 
--- Stores information about aircraft routes
-CREATE TABLE route (
-    -- at least 2 characters
-    carrier_code VARCHAR(3) NOT NULL, 
-    -- the associated number of the flight
-    flight_num SMALLINT,
-    -- scheduled time of departure
-    depart_time TIMESTAMP, 
-    -- scheduled time of arrival
-    arrival_time TIMESTAMP, 
-    -- ID of entry containing delay information
-    delay_info_id INT NOT NULL,
-    -- total calculated sum of delay times
-    total_delay INT,
-    -- tail number of the aircraft
-    tail_num VARCHAR(6) NOT NULL,
-    -- stores if the flight was cancelled
-    is_cancelled BOOLEAN,
-    -- 3 digit IATA airport code of the origin airport
-    origin_code CHAR(3) NOT NULL,
-    -- 3 digit IATA airport code of the destination airport
-    destination_code CHAR(3) NOT NULL,
-    -- distance traveled by the route in miles
-    distance INT, 
-    -- day of the week of departure
-    day_of_week TINYINT,
-    -- the carrier code, flight number, and departure time
-    -- will be used to uniqely identify routes
-    PRIMARY KEY (carrier_code, flight_num, depart_time),
-    FOREIGN KEY (carrier_code) REFERENCES airline(carrier_code),
-    FOREIGN KEY (tail_num) REFERENCES plane(tail_num),
-    FOREIGN KEY (delay_info_id) REFERENCES delay_info(delay_info_id),
-    FOREIGN KEY (origin_code, destination_code) REFERENCES airport(port_code)
-);
 
 -- Stores information about different aircrafts
 CREATE TABLE plane (
@@ -71,8 +37,6 @@ CREATE TABLE plane (
 
 -- Stores information about delays
 CREATE TABLE delay_info (
-    -- generated ID for delay_info entries
-    delay_info_id INT AUTO_INCREMENT,
     -- delay at departure in minutes
     departure_delay INT, 
     -- delay at arrival in minutes
@@ -87,6 +51,8 @@ CREATE TABLE delay_info (
     NAS_delay INT,
     -- delay due to security in minutes
     security_delay INT,
+    -- generated ID for delay_info entries
+    delay_info_id INT AUTO_INCREMENT,
     -- the delay_info id will be used to uniquely identify delay information
     PRIMARY KEY (delay_info_id)
 );
@@ -98,15 +64,51 @@ CREATE TABLE airport (
     -- name of the airport 
     port_name VARCHAR(100),
     -- country of the airport
-    port_country VARCHAR(100),
-    -- state of the airport (all flights will be domestic)
-    port_state CHAR(2),
+    port_country CHAR(3),
     -- city of the airport
     port_city VARCHAR(50),
+    -- state of the airport (all flights will be domestic)
+    port_state CHAR(2),
     -- latitudinal coordinate of airport
     port_lat DOUBLE,
     -- longitudinial coordinate of airport
     port_long DOUBLE,
     -- the airport IATA code will be used to uniquely identify airports
     PRIMARY KEY (port_code)
+);
+
+-- Stores information about aircraft routes
+CREATE TABLE route (
+    -- at least 2 characters
+    carrier_code VARCHAR(3) NOT NULL, 
+    -- the associated number of the flight
+    flight_num SMALLINT,
+    -- scheduled time of departure
+    depart_time TIMESTAMP, 
+    -- scheduled time of arrival
+    arrival_time TIMESTAMP,
+    -- total calculated sum of delay times
+    total_delay INT DEFAULT NULL,
+    -- tail number of the aircraft
+    tail_num VARCHAR(6) NOT NULL,
+    -- stores if the flight was cancelled
+    is_cancelled BOOLEAN,
+    -- 3 digit IATA airport code of the origin airport
+    origin_code CHAR(3) NOT NULL,
+    -- 3 digit IATA airport code of the destination airport
+    destination_code CHAR(3) NOT NULL,
+    -- distance traveled by the route in miles
+    distance INT, 
+    -- day of the week of departure
+    day_of_week TINYINT,
+    -- ID of entry containing delay information
+    delay_info_id INT AUTO_INCREMENT NOT NULL,
+    -- the carrier code, flight number, and departure time
+    -- will be used to uniqely identify routes
+    PRIMARY KEY (carrier_code, flight_num, depart_time),
+    FOREIGN KEY (carrier_code) REFERENCES airline(carrier_code),
+    FOREIGN KEY (tail_num) REFERENCES plane(tail_num),
+    FOREIGN KEY (delay_info_id) REFERENCES delay_info(delay_info_id),
+    FOREIGN KEY (origin_code) REFERENCES airport(port_code),
+    FOREIGN KEY (destination_code) REFERENCES airport(port_code)
 );
